ad070fa3735e1d2f49110505166733195d9f119e
sasmirno <sasmirno1@gmail.com>
2022-04-20 20:42:50 +0300

    Чутка допилил джаваскрипт

--- script.js
+++ script.js
@@ -125,28 +125,31 @@ function visibility(show, hide) {
 	document.getElementById(show).style.visibility = "hidden";
 }
 /*-----*/
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+var dot = ["dot1", "dot2"]
+showSlides(1, 0);
+showSlides(1, 1);
 // Next/previous controls
-function plusSlides(n) {
-	showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 // Thumbnail image controls
-function currentSlide(n) {
-	showSlides(slideIndex = n);
+function currentSlide(n, no) {
+	showSlides(slideIndex[no] = n, no);
 }
-function showSlides(n) {
+function showSlides(n, no) {
 	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
+	var slides = document.getElementsByClassName(slideId[no]);
+	var dots = document.getElementsByClassName(dot[no]);
+	if (n > slides.length) {slideIndex[no] = 1}
+	if (n < 1) {slideIndex[no] = slides.length}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" active", "");
 	}
-	slides[slideIndex-1].style.display = "flex";
-	dots[slideIndex-1].className += " active";
+	slides[slideIndex[no]-1].style.display = "flex";
+	dots[slideIndex[no]-1].className += " active";
 }
