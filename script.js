8d8726543e4c38c5755afef12e10f56cea49625c
sasmirno <sasmirno1@gmail.com>
2022-04-23 21:36:06 +0300

    Прикрутил слайдер к попапу

--- script.js
+++ script.js
@@ -153,3 +153,22 @@ function showSlides(n, no) {
 	slides[slideIndex[no]-1].style.display = "flex";
 	dots[slideIndex[no]-1].className += " active";
 }
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+function plusSlides2(n) {
+	showSlides2(slideIndex2 += n);
+}
+function currentSlide2(n) {
+	showSlides2(slideIndex2 = n);
+}
+function showSlides2(n) {
+	var i;
+	var slides = document.getElementsByClassName("slides");
+	if (n > slides.length) {slideIndex2 = 1}
+	if (n < 1) {slideIndex2 = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex2-1].style.display = "block";
+}
